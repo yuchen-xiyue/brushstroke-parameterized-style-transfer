824acbc0b9be5c337215307281e0d88974a10c04
yuchen-xiyue <yw3283@bath.ac.uk>
2023-03-18 20:27:19 +0000

    change map initiating to inplace

--- spatial_control_model.py
+++ spatial_control_model.py
@@ -189,31 +189,17 @@ class BrushstrokeOptimizer:
         # Adding maps
         content_maps = [content_map.resize((self.canvas_width, self.canvas_height)) for content_map in content_maps]
         content_maps = [np.array(content_map).astype(self.dtype) for content_map in content_maps]
-        content_maps1_1 = [np.array(cm) for cm in content_maps]
-        content_maps2_1 = [np.array(cm) for cm in content_maps]
-        content_maps3_1 = [np.array(cm) for cm in content_maps]
-        content_maps4_1 = [np.array(cm) for cm in content_maps]
-        content_maps5_1 = [np.array(cm) for cm in content_maps]
-        self.content_maps = {
-            'conv1_1': [cm.resize((self.canvas_width//2, self.canvas_height//2), refcheck=False) for cm in content_maps1_1], 
-            'conv2_1': [cm.resize((self.canvas_width//4, self.canvas_height//4), refcheck=False) for cm in content_maps2_1], 
-            'conv3_1': [cm.resize((self.canvas_width//8, self.canvas_height//8), refcheck=False) for cm in content_maps3_1], 
-            'conv4_1': [cm.resize((self.canvas_width//16, self.canvas_height//16), refcheck=False) for cm in content_maps4_1], 
-            'conv5_1': [cm.resize((self.canvas_width//32, self.canvas_height//32), refcheck=False) for cm in content_maps5_1]}
+        self.content_maps = {}
+        for i in range(1, 6): 
+            for cm in content_maps: cm.resize((self.canvas_width//2**i, self.canvas_height//2**i), refcheck=False)
+            self.content_maps['conv'+str(i)+'_1'] = [np.array(cm) for cm in content_maps]
 
         style_maps = [style_map.resize((self.canvas_width, self.canvas_height)) for style_map in style_maps]
         style_maps = [np.array(style_map).astype(self.dtype) for style_map in style_maps]
-        style_maps1_1 = [np.array(sm) for sm in style_maps]
-        style_maps2_1 = [np.array(sm) for sm in style_maps]
-        style_maps3_1 = [np.array(sm) for sm in style_maps]
-        style_maps4_1 = [np.array(sm) for sm in style_maps]
-        style_maps5_1 = [np.array(sm) for sm in style_maps]
-        self.style_maps = {
-            'conv1_1': [sm.resize((self.canvas_width//2, self.canvas_height//2), refcheck=False) for sm in style_maps2_1], 
-            'conv2_1': [sm.resize((self.canvas_width//4, self.canvas_height//4), refcheck=False) for sm in style_maps2_1], 
-            'conv3_1': [sm.resize((self.canvas_width//8, self.canvas_height//8), refcheck=False) for sm in style_maps3_1], 
-            'conv4_1': [sm.resize((self.canvas_width//16, self.canvas_height//16), refcheck=False) for sm in style_maps4_1], 
-            'conv5_1': [sm.resize((self.canvas_width//32, self.canvas_height//32), refcheck=False) for sm in style_maps5_1]}
+        self.style_maps = {}
+        for i in range(1, 6): 
+            for sm in style_maps: sm.resize((self.canvas_width//2**i, self.canvas_height//2**i), refcheck=False)
+            self.style_maps['conv'+str(i)+'_1'] = [np.array(sm) for sm in style_maps]
 
         if draw_curve_position_path is not None and draw_curve_vector_path is not None:
             self.draw_curve_position_np = np.load(draw_curve_position_path)
@@ -420,32 +406,17 @@ class PixelOptimizer:
         # Adding maps
         content_maps = [content_map.resize((self.canvas_width, self.canvas_height)) for content_map in content_maps]
         content_maps = [np.array(content_map).astype(self.dtype) for content_map in content_maps]
-        content_maps1_1 = [np.array(cm) for cm in content_maps]
-        content_maps2_1 = [np.array(cm) for cm in content_maps]
-        content_maps3_1 = [np.array(cm) for cm in content_maps]
-        content_maps4_1 = [np.array(cm) for cm in content_maps]
-        content_maps5_1 = [np.array(cm) for cm in content_maps]
-        self.content_maps = {
-            'conv1_1': [cm.resize((self.canvas_width//2, self.canvas_height//2), refcheck=False) for cm in content_maps1_1], 
-            'conv2_1': [cm.resize((self.canvas_width//4, self.canvas_height//4), refcheck=False) for cm in content_maps2_1], 
-            'conv3_1': [cm.resize((self.canvas_width//8, self.canvas_height//8), refcheck=False) for cm in content_maps3_1], 
-            'conv4_1': [cm.resize((self.canvas_width//16, self.canvas_height//16), refcheck=False) for cm in content_maps4_1], 
-            'conv5_1': [cm.resize((self.canvas_width//32, self.canvas_height//32), refcheck=False) for cm in content_maps5_1]}
- 
+        self.content_maps = {}
+        for i in range(1, 6): 
+            for cm in content_maps: cm.resize((self.canvas_width//2**i, self.canvas_height//2**i), refcheck=False)
+            self.content_maps['conv'+str(i)+'_1'] = [np.array(cm) for cm in content_maps]
+
         style_maps = [style_map.resize((self.canvas_width, self.canvas_height)) for style_map in style_maps]
         style_maps = [np.array(style_map).astype(self.dtype) for style_map in style_maps]
-        style_maps1_1 = [np.array(sm) for sm in style_maps]
-        style_maps2_1 = [np.array(sm) for sm in style_maps]
-        style_maps3_1 = [np.array(sm) for sm in style_maps]
-        style_maps4_1 = [np.array(sm) for sm in style_maps]
-        style_maps5_1 = [np.array(sm) for sm in style_maps]
-        self.style_maps = {
-            'conv1_1': [sm.resize((self.canvas_width//2, self.canvas_height//2), refcheck=False) for sm in style_maps2_1], 
-            'conv2_1': [sm.resize((self.canvas_width//4, self.canvas_height//4), refcheck=False) for sm in style_maps2_1], 
-            'conv3_1': [sm.resize((self.canvas_width//8, self.canvas_height//8), refcheck=False) for sm in style_maps3_1], 
-            'conv4_1': [sm.resize((self.canvas_width//16, self.canvas_height//16), refcheck=False) for sm in style_maps4_1], 
-            'conv5_1': [sm.resize((self.canvas_width//32, self.canvas_height//32), refcheck=False) for sm in style_maps5_1]}
-
+        self.style_maps = {}
+        for i in range(1, 6): 
+            for sm in style_maps: sm.resize((self.canvas_width//2**i, self.canvas_height//2**i), refcheck=False)
+            self.style_maps['conv'+str(i)+'_1'] = [np.array(sm) for sm in style_maps]
         self.canvas_np = canvas
         self.content_img_np = canvas
         self.style_img_np = style_img
