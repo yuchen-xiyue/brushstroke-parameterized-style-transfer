78d700c33450caa9bbbc424cd7c094431886bb84
yuchen-xiyue <yw3283@bath.ac.uk>
2023-03-18 18:01:42 +0000

    add running option

--- main.py
+++ main.py
@@ -1,22 +1,61 @@
 from PIL import Image
 import model
-import sys
-sys.path.append('/usr/local/lib/python3.5/dist-packages')
-cont_imgs = ['elefant.jpg', 'cabin.jpg']#'tree.jpg']#'church.jpg']#'golden_gate.jpg'] 
+import spatial_control_model
+import os
+from os import path
+import argparse
+import glob
+# import sys
+# sys.path.append('/usr/local/lib/python3.5/dist-packages')
+CWD = os.getcwd()
+IMGS_PATH = path.join(CWD, 'images')
+MAPS_PATH = path.join(IMGS_PATH, 'maps')
+CONT_PATH = path.join(IMGS_PATH, 'content')
+STYL_PATH = path.join(IMGS_PATH, 'style')
+
+cont_imgs = ['elefant.jpg', 'cabin.jpg']#'tree.jpg']#'church.jpg']#'golden_gate.jpg'] 
 styl_imgs = ['scream.jpg' , 'van_gogh_trees.jpg']#'street.jpg']#'van_gogh_starry_night.jpg']
 
-for i in range(len(cont_imgs)): 
+parser = argparse.ArgumentParser()
+parser.add_argument('-g', '--guided')
+args = parser.parse_args()
 
-	content_img = Image.open('images/content/'+cont_imgs[i])
-	style_img   = Image.open('images/style/'+styl_imgs[i])
+if args.guided:
+	for cont_img, styl_img in zip(cont_imgs, styl_imgs):
+		cont_img = Image.open(path.join(CONT_PATH, cont_img))
+		cont_name = cont_img.split('.')[0]
+		cont_map_path = path.join(MAPS_PATH, cont_name)
+		cont_maps = [Image.open(filepath) for filepath in glob.glob(path.join(cont_map_path, '[0-9]*.*'))]
 
-	stylized_img = model.stylize(content_img,
-							style_img,
-							num_strokes=5000,
-							num_steps=100,
-							content_weight=1.0,
-							style_weight=3.0,
-							num_steps_pixel=1000)
-	cont_name = cont_imgs[i].split('.')[0]
-	styl_name = styl_imgs[i].split('.')[0]
-	stylized_img.save('images/'+cont_name+'_stylized_by_'+styl_name+'.jpg')
+		styl_img = Image.open(path.join(STYL_PATH, styl_img))
+		styl_name = styl_img.split('.')[0]
+		styl_map_path = path.join(MAPS_PATH, styl_name)
+		styl_maps = [Image.open(filepath) for filepath in glob.glob(path.join(styl_map_path, '[0-9]*.*'))]
+
+		stylized_img = spatial_control_model.stylize(cont_img,
+													 styl_img,
+													 cont_maps,
+													 styl_maps,
+													 num_strokes=5000,
+													 num_steps=100,
+													 content_weight=1.0,
+													 style_weight=3.0,
+													 num_steps_pixel=1000)
+		stylized_img.save(path.join(IMGS_PATH, cont_name+'_stylized_by_'+styl_name+'_guided.jpg'))
+
+else:
+	for cont_img, styl_img in zip(cont_imgs, styl_imgs):
+
+		cont_img = Image.open(path.join(CONT_PATH, cont_img))
+		styl_img = Image.open(path.join(STYL_PATH, styl_img))
+		cont_name = cont_img.split('.')[0]
+		styl_name = styl_img.split('.')[0]
+
+		stylized_img = model.stylize(cont_img,
+								styl_img,
+								num_strokes=5000,
+								num_steps=100,
+								content_weight=1.0,
+								style_weight=3.0,
+								num_steps_pixel=1000)
+		stylized_img.save(path.join(IMGS_PATH, cont_name+'_stylized_by_'+styl_name+'.jpg'))
