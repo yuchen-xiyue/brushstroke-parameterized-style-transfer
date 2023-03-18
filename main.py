865e318319e0afcf699a40f35fba12f0d8fcde49
yuchen-xiyue <yw3283@bath.ac.uk>
2023-03-18 18:36:37 +0000

    Modified

--- main.py
+++ main.py
@@ -17,18 +17,19 @@ cont_imgs = ['tree.jpg']#'elefant.jpg', 'cabin.jpg']##'church.jpg']#'golden_gate
 styl_imgs = ['street.jpg']#'scream.jpg' , 'van_gogh_trees.jpg']##'van_gogh_starry_night.jpg']
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-g', '--guided')
+parser.add_argument('-g', '--guided', action='store_true')
+parser.set_defaults(guided=False)
 args = parser.parse_args()
 
 if args.guided:
-	for cont_img, styl_img in zip(cont_imgs, styl_imgs):
-		cont_img = Image.open(path.join(CONT_PATH, cont_img))
-		cont_name = cont_img.split('.')[0]
+	for cont_name, styl_name in zip(cont_imgs, styl_imgs):
+		cont_img = Image.open(path.join(CONT_PATH, cont_name))
+		cont_name = cont_name.split('.')[0]
 		cont_map_path = path.join(MAPS_PATH, cont_name)
 		cont_maps = [Image.open(filepath) for filepath in glob.glob(path.join(cont_map_path, '[0-9]*.*'))]
 
-		styl_img = Image.open(path.join(STYL_PATH, styl_img))
-		styl_name = styl_img.split('.')[0]
+		styl_img = Image.open(path.join(STYL_PATH, styl_name))
+		styl_name = styl_name.split('.')[0]
 		styl_map_path = path.join(MAPS_PATH, styl_name)
 		styl_maps = [Image.open(filepath) for filepath in glob.glob(path.join(styl_map_path, '[0-9]*.*'))]
 
@@ -44,12 +45,12 @@ if args.guided:
 		stylized_img.save(path.join(IMGS_PATH, cont_name+'_stylized_by_'+styl_name+'_guided.jpg'))
 
 else:
-	for cont_img, styl_img in zip(cont_imgs, styl_imgs):
+	for cont_name, styl_name in zip(cont_imgs, styl_imgs):
 
-		cont_img = Image.open(path.join(CONT_PATH, cont_img))
-		styl_img = Image.open(path.join(STYL_PATH, styl_img))
-		cont_name = cont_img.split('.')[0]
-		styl_name = styl_img.split('.')[0]
+		cont_img = Image.open(path.join(CONT_PATH, cont_name))
+		styl_img = Image.open(path.join(STYL_PATH, styl_name))
+		cont_name = cont_name.split('.')[0]
+		styl_name = styl_name.split('.')[0]
 
 		stylized_img = model.stylize(cont_img,
 								styl_img,
